8fe2d60769a535b7bc019d4ddf7cb3dd94ba9523
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-11-16 18:36:08 +0100

    implementazione della struttura base della classe ArticoloNonAlimentare derivata dalla classe Articolo.
    sono stati implementati costruttori, properties, ToString(), Equals()

--- 10 - Classe articoli/ArticoloNonAlimentare.cs
+++ 10 - Classe articoli/ArticoloNonAlimentare.cs
@@ -8,6 +8,60 @@ namespace _10___Classe_articoli
 {
     public class ArticoloNonAlimentare : Articolo
     {
+        private string _materiale;
+        private bool _riciclabile;
 
+        public string Materiale
+        {
+            get { return _materiale; }
+            set { _materiale = value; }
+        }
+
+        public bool Riciclabile
+        {
+            get { return _riciclabile; }
+            set { _riciclabile = value; }
+        }
+
+        public ArticoloNonAlimentare() : base()
+        {
+            Materiale = "";
+            Riciclabile = false;
+        }
+
+        public ArticoloNonAlimentare(string materiale, bool riciclabile, int codice, string descrizione, double prezzoUnit, bool cartaFed) : base(codice, descrizione, prezzoUnit, cartaFed)
+        {
+            Materiale = materiale;
+            Riciclabile = riciclabile;
+        }
+
+        public ArticoloNonAlimentare(ArticoloNonAlimentare vecchioArtNonAlim, Articolo vecchioArt) : base(vecchioArt)
+        {
+            Materiale = vecchioArtNonAlim.Materiale;
+            Riciclabile = vecchioArtNonAlim.Riciclabile;
+        }
+
+        public override string ToString()
+        {
+            string str = Riciclabile ? "Riciclabile" : "Non riciclabile";
+            return $"{base.ToString()}; Materiale: {Materiale}; Riciclabilità: {str}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            ArticoloNonAlimentare other = (ArticoloNonAlimentare)obj;
+            
+            if (base.Equals(other) && Materiale == other.Materiale && Riciclabile == other.Riciclabile)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
