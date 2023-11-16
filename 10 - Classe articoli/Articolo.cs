a49c0b34e9ae589098d2be5a49b73383dd6a31e0
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-11-16 10:11:17 +0100

    Aggiungere i file di progetto.

---
+++ 10 - Classe articoli/Articolo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___Classe_articoli
+{
+    public class Articolo
+    {
+
+    }
+
+    public class ArticoloAlimentare : Articolo
+    {
+
+    }
+
+    public class ArticoloNonAlimentare : Articolo
+    {
+
+    }
+
+    public class ArticoloAlimentareFresco : ArticoloAlimentare
+    {
+
+    }
+}
