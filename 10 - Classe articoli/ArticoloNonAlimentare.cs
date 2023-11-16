5400ee451ba3c3ba3a4cb6a49fcff5d0165a480a
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-11-16 18:39:11 +0100

    implementazione del metodo Sconta() della classe ArticoloNonAlimentare derivata dalla classe Articolo

--- 10 - Classe articoli/ArticoloNonAlimentare.cs
+++ 10 - Classe articoli/ArticoloNonAlimentare.cs
@@ -63,5 +63,39 @@ namespace _10___Classe_articoli
 
             return false;
         }
+
+        public override double Sconta()
+        {
+            // Se si considera lo sconto come: 10% statico del totale
+            // return PrezzoUnit - PrezzoUnit * (10 / 100);
+
+            // Se si considera lo sconto come: (10 + 20)% del totale
+            // return PrezzoUnit - PrezzoUnit * (30 / 100);
+
+            // Se si considera lo sconto come: 10% applicato al 5% del totale
+            // return PrezzoUnit - base.Sconta() * (10 / 100);
+
+
+            // Se ho sia la carta che la condizione dell'anno
+            if ((CartaFed && Riciclabile) && (CartaFed))
+            {
+                return PrezzoUnit - base.Sconta() * (10 / 100);
+            }
+
+            // Se ho solo la carta
+            if ((CartaFed && Riciclabile) == false && (CartaFed))
+            {
+                base.Sconta();
+            }
+            
+            // Se ho solo la condizione dell'anno
+            if (Riciclabile && CartaFed == false)
+            {
+                return PrezzoUnit - PrezzoUnit * (10 / 100);
+            }
+
+            // Se non ho nessuna delle due
+            return PrezzoUnit;
+        }
     }
 }
